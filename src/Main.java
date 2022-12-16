67b8bf3b992eb827cdf7ec52632f4b2354c4730f
Troeborec <defied_jeff@mail.ru>
2022-12-17 01:49:16 +1200

    The end!

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         //task 5
         System.out.println("Задача №5");
         for (int year = 1904; year <= 2096 ; year = year + 4) {
-            if (year%4==0 && year%100!=0 || year%400==0){
+            if (year %4 == 0 && year % 100 !=0 || year % 400 == 0){
                 System.out.println(year + " год является високосным");
             }
         }
@@ -57,7 +57,7 @@ public class Main {
         double persent = 0.01;
         for (int i = 1; i < 12; i++) {
             totalWithPerсent = (totalWithPerсent + salary_2) * (1 + persent) ;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalWithPerсent + " рублей");
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalWithPerсent + " рублей ");
         }
     }
 }
